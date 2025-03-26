0e6d664717ab57df9b65fa4e31097ee19002afc3
Razia Prokopiou <razia@al.co.za>
2025-03-27 00:13:52 +0200

    Completed all html pages and, -css added -assests  added -made responsie

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuToggle = document.getElementById('hamburger-icon');
-  const mobileMenu = document.getElementById('mobile-menu');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("hamburger-icon");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-  menuToggle.addEventListener('click', function() {
-    mobileMenu.classList.toggle('active');
-    menuToggle.classList.toggle('open');
+  menuToggle.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+    menuToggle.classList.toggle("open");
   });
 });
